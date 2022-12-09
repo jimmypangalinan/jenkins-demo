4b5e02bc0d2b28f7181091b6b1630d2344799257
Jimmy_Pangalinan <Jimmipangalinan@gmail.com>
2022-12-09 15:02:32 +0700

    update

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/jimmypangalinan/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('jimmypangalinan')
+            node / gitConfigEmail('jimmipangalinan@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('jimmy17/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
